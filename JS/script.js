1acf27c2ddab40f3bcc2bb29ce025b4e8d59cd99
Rinky <71222541+Rinky6767@users.noreply.github.com>
2022-07-02 00:01:20 +0530

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -8,10 +8,7 @@ let display= document.getElementById("resultDisplay");
 let reset= document.getElementById("resetButton").addEventListener("click",reStart);
 
 let going = new Audio("Adventure-320bit.mp3");
-window.onload()
-{
- going.play();
-}
+
 if(localStorage.getItem("Score")!==null){
     hg= localStorage.getItem("Score");
     highestScore.textContent= "Highest Score: "+hg;
@@ -22,6 +19,7 @@ function reStart(){
     window.location.reload();
 }
 function createTable() {
+   going.play();
     let Continer = document.createElement("table");
     document.body.appendChild(Continer);
     let countCell = -1;
